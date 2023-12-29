55df8bd5a32b6290e2b2e8da5138e1ffda23adf8
gredia <flamefairy.gredia@gmail.com>
2023-12-30 00:18:24 +0900

    seach custom

--- app/chewy/public_statuses_index.rb
+++ app/chewy/public_statuses_index.rb
@@ -5,11 +5,6 @@ class PublicStatusesIndex < Chewy::Index
 
   settings index: index_preset(refresh_interval: '30s', number_of_shards: 5), analysis: {
     filter: {
-      english_stop: {
-        type: 'stop',
-        stopwords: '_english_',
-      },
-
       english_stemmer: {
         type: 'stemmer',
         language: 'english',
@@ -21,23 +16,35 @@ class PublicStatusesIndex < Chewy::Index
       },
     },
 
-    analyzer: {
-      verbatim: {
-        tokenizer: 'uax_url_email',
-        filter: %w(lowercase),
+    tokenizer: {
+      ja_tokenizer: {
+        type: 'kuromoji_tokenizer',
+        mode: 'search',
+        user_dictionary: 'userdict_ja.txt',
       },
+    },
 
+    analyzer: {
       content: {
-        tokenizer: 'standard',
+        tokenizer: 'ja_tokenizer',
+        type: 'custom',
+        char_filter: %w(
+          icu_normalizer
+        ),
         filter: %w(
+          kuromoji_stemmer
+          kuromoji_part_of_speech
+          english_possessive_stemmer
           lowercase
           asciifolding
           cjk_width
           elision
-          english_possessive_stemmer
-          english_stop
           english_stemmer
         ),
+      },
+
+      ja_default_analyzer: {
+        tokenizer: 'kuromoji_tokenizer',
       },
 
       hashtag: {
@@ -55,12 +62,12 @@ class PublicStatusesIndex < Chewy::Index
   index_scope ::Status.unscoped
                       .kept
                       .indexable
-                      .includes(:media_attachments, :preloadable_poll, :preview_cards, :tags)
+                      .includes(:media_attachments, :preloadable_poll, :tags, preview_cards_status: :preview_card)
 
   root date_detection: false do
     field(:id, type: 'long')
     field(:account_id, type: 'long')
-    field(:text, type: 'text', analyzer: 'verbatim', value: ->(status) { status.searchable_text }) { field(:stemmed, type: 'text', analyzer: 'content') }
+    field(:text, type: 'text', analyzer: 'ja_default_analyzer', value: ->(status) { status.searchable_text }) { field(:stemmed, type: 'text', analyzer: 'content') }
     field(:tags, type: 'text', analyzer: 'hashtag', value: ->(status) { status.tags.map(&:display_name) })
     field(:language, type: 'keyword')
     field(:properties, type: 'keyword', value: ->(status) { status.searchable_properties })
--- app/chewy/statuses_index.rb
+++ app/chewy/statuses_index.rb
@@ -5,11 +5,6 @@ class StatusesIndex < Chewy::Index
 
   settings index: index_preset(refresh_interval: '30s', number_of_shards: 5), analysis: {
     filter: {
-      english_stop: {
-        type: 'stop',
-        stopwords: '_english_',
-      },
-
       english_stemmer: {
         type: 'stemmer',
         language: 'english',
@@ -21,23 +16,35 @@ class StatusesIndex < Chewy::Index
       },
     },
 
-    analyzer: {
-      verbatim: {
-        tokenizer: 'uax_url_email',
-        filter: %w(lowercase),
+    tokenizer: {
+      ja_tokenizer: {
+        type: 'kuromoji_tokenizer',
+        mode: 'search',
+        user_dictionary: 'userdict_ja.txt',
       },
+    },
 
+    analyzer: {
       content: {
-        tokenizer: 'standard',
+        tokenizer: 'ja_tokenizer',
+        type: 'custom',
+        char_filter: %w(
+          icu_normalizer
+        ),
         filter: %w(
+          kuromoji_stemmer
+          kuromoji_part_of_speech
+          english_possessive_stemmer
           lowercase
           asciifolding
           cjk_width
           elision
-          english_possessive_stemmer
-          english_stop
           english_stemmer
         ),
+      },
+
+      ja_default_analyzer: {
+        tokenizer: 'kuromoji_tokenizer',
       },
 
       hashtag: {
@@ -52,13 +59,13 @@ class StatusesIndex < Chewy::Index
     },
   }
 
-  index_scope ::Status.unscoped.kept.without_reblogs.includes(:media_attachments, :preview_cards, :local_mentioned, :local_favorited, :local_reblogged, :local_bookmarked, :tags, preloadable_poll: :local_voters), delete_if: ->(status) { status.searchable_by.empty? }
+  index_scope ::Status.unscoped.kept.without_reblogs.includes(:media_attachments, :local_mentioned, :local_favorited, :local_reblogged, :local_bookmarked, :tags, preview_cards_status: :preview_card, preloadable_poll: :local_voters), delete_if: ->(status) { status.searchable_by.empty? }
 
   root date_detection: false do
     field(:id, type: 'long')
     field(:account_id, type: 'long')
-    field(:text, type: 'text', analyzer: 'verbatim', value: ->(status) { status.searchable_text }) { field(:stemmed, type: 'text', analyzer: 'content') }
-    field(:tags, type: 'text', analyzer: 'hashtag',  value: ->(status) { status.tags.map(&:display_name) })
+    field(:text, type: 'text', analyzer: 'ja_default_analyzer', value: ->(status) { status.searchable_text }) { field(:stemmed, type: 'text', analyzer: 'content') }
+    field(:tags, type: 'text', analyzer: 'hashtag', value: ->(status) { status.tags.map(&:display_name) })
     field(:searchable_by, type: 'long', value: ->(status) { status.searchable_by })
     field(:language, type: 'keyword')
     field(:properties, type: 'keyword', value: ->(status) { status.searchable_properties })
