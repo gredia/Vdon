e99dba504a2674d284a7bde782a8b1e56b360168
gredia <flamefairy.gredia@gmail.com>
2024-02-19 03:37:01 +0900

    Reject条件を変更3day->7day

--- app/lib/activitypub/activity/create.rb
+++ app/lib/activitypub/activity/create.rb
@@ -440,7 +440,7 @@ class ActivityPub::Activity::Create < ActivityPub::Activity
     (
       !@status.account.local? &&
       @status.account.followers_count.zero? &&
-      @status.account.created_at > 3.day.ago &&
+      @status.account.created_at > 7.day.ago &&
       @mentions.count >= 2
     )
   end
