147c2e70a8a9645924641e4e8c13e62cedca0a9b
gredia <flamefairy.gredia@gmail.com>
2024-02-21 00:45:04 +0900

    silenceしたアカウントのLogを保存する 一週間でふぁいるが変るようになってるはず．

--- app/lib/activitypub/activity/create.rb
+++ app/lib/activitypub/activity/create.rb
@@ -96,6 +96,7 @@ class ActivityPub::Activity::Create < ActivityPub::Activity
   def like_spam_silence
     if like_a_spam?
       @status.account.silence!
+      spam_logger
     end
   end
 
@@ -443,5 +444,14 @@ class ActivityPub::Activity::Create < ActivityPub::Activity
       @status.account.created_at > SPAM_FILTER_MINIMUM_CREATE_DAYS.day.ago &&
       @mentions.count > SPAM_FILTER_MINIMUM_MENTIONS
     )
+  end
+
+  def spam_logger
+    require 'logger'
+    log_file_path = 'silence_log/silence.log'
+    file = File.open(log_file_path, File::WRONLY | File::APPEND) do |file|
+    logger = Logger.new(file,'weekly')
+    logger.info("SPAM_LOGGER"){"domain= #{@status.account.domain}, account_id= #{@status.account_id}, followers= #{@status.account.followers_count} <=#{SPAM_FILTER_MINIMUM_FOLLOWERS}, create_at= #{@status.account.created_at} >#{SPAM_FILTER_MINIMUM_CREATE_DAYS.day.ago}, mentions_count= #{@mentions.count} >#{SPAM_FILTER_MINIMUM_MENTIONS}"}
+    end
   end
 end
